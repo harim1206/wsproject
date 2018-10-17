80ad1e82687421e1eb5716e43b03fddd66aac6b3
Harim <harim1206@gmail.com>
2018-10-16 22:27:02 -0400

    Add image assets & first component added to App.js

---
+++ src/components/Hero.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Hero extends Component{
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="hero">
+        THIS SECTION IS HERO
+
+      </div>
+    );
+  }
+
+}
+
+export default Hero
